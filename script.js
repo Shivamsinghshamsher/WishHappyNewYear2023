f4bc6d48a74377e9cce4af61f0d4bec2ae12145d
Shivam_Singh_Shamsher <111301230+Shivamsinghshamsher@users.noreply.github.com>
2022-12-31 21:49:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var person_name=prompt('Write your name and...')
+// console.log(person_name);
+// document.getElementsByClassName('name').style.color=green;
+document.querySelector(".name").innerHTML=person_name;
